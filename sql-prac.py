5549a9939cbf682eb5fe4efc8d86d6d9399f7d4b
Stephen <sdarcy29@hotmail.com>
2022-01-20 20:16:48 +0000

    DB practice

--- sql-prac.py
+++ sql-prac.py
@@ -12,10 +12,14 @@ base = declarative_base()
 
 
 #  Create a class based model for the programmer table.
-
-
-
-
+class Games(base):
+    __tablename__ = "Games"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    genre = Column(String)
+    console = Column(String)
+    age = Column(Integer, primary_key=False)
+    
 
 # instead of connecting to the database directly, we will ask for a session
 # create a new instance of sessionmaker, then point to our engine (the db)
@@ -27,3 +31,41 @@ session = Session()
 
 # creating the database using declarative_base subclass
 base.metadata.create_all(db)
+
+
+demon_souls = Games(
+    name = "Demon Souls",
+    genre = "Horror",
+    console = "Playstation",
+    age = 15,
+)
+
+final_fantasy = Games(
+    name = "Final Fantasy",
+    genre = "Sci-Fi",
+    console = "Playstation",
+    age = 12,
+)
+
+
+# add each instance of our programmers session
+session.add(demon_souls)
+session.add(final_fantasy)
+
+
+
+# Commit our session to the database
+session.commit()
+
+
+# Query the database to find all programmers
+game = session.query(Games)
+for gamer in game:
+    print(
+        gamer.id,
+        gamer.name,
+        gamer.genre,
+        gamer.console,
+        gamer.age,
+        sep=" | "
+    )
